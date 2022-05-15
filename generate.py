96d9f164dc068eb018622be47a4374f2d641c87e
Airmole <admin@airmole.cn>
2022-05-16 03:38:37 +0800

    Initial commit

---
+++ generate.py
@@ -0,0 +1,124 @@
+import json
+import os
+import jieba
+import stylecloud
+
+def main(file_dir):
+    for root, dirs, files in os.walk(file_dir):
+        # print(root)  # 当前目录路径
+        # print(dirs)  # 当前路径下所有子目录
+        # print(files)  # 当前路径下所有非目录子文件
+        for file in files:
+            data = readDatas(file_dir+'/'+file)
+            userRank = analyseUserTop(data)
+            # print(userTop)
+            timeline = analyseTime(data)
+            # print(time)
+            pie = analysePie(userRank)
+            wordcloud(file, data)
+            save2Js(file, userRank, timeline, pie)
+
+def save2Js(filename, userRank, timeline, pie):
+    result = {}
+    filename = filename[13:21] + '.json'
+    outputPath = './output/'
+    result['user_rank'] = userRank
+    result['timeline'] = timeline
+    result['pie'] = pie
+    jsonStr = json.dumps(result)
+    fullpath = outputPath+filename
+    file = open(fullpath, 'w')
+    file.write(jsonStr)
+    file.close()
+    print(fullpath + '文件已经创建')
+
+
+def readDatas(file):
+    with open(file, 'r', encoding='utf-8') as lines:
+        array = lines.readlines()  #返回的是一个列表，该列表每一个元素是txt文件的每一行
+        datas = [] #使用一个新的列表来装去除换行符\n后的数据
+        for i in array: #遍历array中的每个元素
+            dict = {}
+            time = i[4:9]
+            dict['time'] = time
+            nicknameStart = i.find(' : ') + 3
+            nicknameEnd = i.find(' 说：')
+            nickname = i[nicknameStart:nicknameEnd]
+            dict['nickname'] = nickname
+            content = i[nicknameEnd+3:]
+            dict['content'] = content
+            datas.append(dict)
+        return datas
+
+def analyseUserTop(datas):
+    result = {}
+    analyse = {}
+    for data in datas:
+        isIn = data['nickname'] in analyse.keys()
+        if not isIn:
+            analyse[data['nickname']] = 0
+    for data in datas:
+        isIn = data['nickname'] in analyse.keys()
+        if isIn:
+            analyse[data['nickname']] += 1
+        else:
+            analyse[data['nickname']] = 0
+    result['label'] = list(analyse.keys())
+    result['value'] = list(analyse.values())
+    return result
+
+def analyseTime(datas):
+    res = {}
+    result = {}
+    for item in range(24):
+            result[str(item)+':00'] = 0
+    for data in datas:
+        hour = data['time'][0:2]+':00'
+        isIn = hour in result.keys()
+        if isIn:
+            result[hour] += 1
+        else:
+            result[hour] = 0
+    res['label'] = list(result.keys())
+    res['value'] = list(result.values())
+    return res
+
+def analysePie(userRank):
+    result = []
+    for index, item in enumerate(userRank['label']):
+        row = {}
+        limit = 3
+        if index > limit-1:
+            if len(result) == limit:
+                row['name'] = '其他用户'
+                row['value'] = userRank['value'][index]
+                result.append(row)
+            else:
+                result[limit]['name'] = '其他用户'
+                result[limit]['value'] = result[limit]['value'] + userRank['value'][index]
+            continue
+        row['name'] = userRank['label'][index]
+        row['value'] = userRank['value'][index]
+        result.append(row)
+    return result
+
+def wordcloud(filename, datas):
+    filename = filename[13:21] + '.png'
+    outputPath = './wordcloud/'
+    allContent = ''
+    for data in datas:
+        allContent += data['content']
+    word_list = jieba.cut(allContent)
+    result = " ".join(word_list)  # 分词用空格隔开
+    stylecloud.gen_stylecloud(
+        text=result,  # 上面分词的结果作为文本传给text参数
+        size=512,
+        font_path='msyh.ttc',  # 字体设置
+        palette='cartocolors.qualitative.Pastel_7',  # 调色方案选取，从palettable里选择
+        gradient='horizontal',  # 渐变色方向选了垂直方向
+        icon_name='fas fa-heart',  # 蒙版选取，从Font Awesome里选
+        output_name=outputPath+filename)  # 输出词云图
+
+
+dataPath = './data'
+main(dataPath)
